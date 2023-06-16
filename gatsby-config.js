038a83b52ac3fa94bc769978b91c26303299967e
Evan Boettner <eboettner@icstars.org>
2023-06-15 21:34:21 -0500

    mdx and queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,13 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`, //The name option in the configuration object gets set to the sourceInstanceName field for each file. This comes in handy when you want to source files from multiple folders. By giving each folder a different name option, you can build GraphQL queries that filter down to only a particular folder.
+        path: `${__dirname}/blog/`, //this is a Node.Js variable that stores the absolute path for the directory that contains the file currently being run
+      },
+    },
+    "gatsby-plugin-mdx",
   ],
-}
+};
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,38 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
+import { graphql } from 'gatsby';
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
    return (
     <Layout pageTitle="My Blog Posts">
-        <p>My cool posts will go in here</p>
+            {
+                data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>{node.excerpt}</p>
+                    </article>
+                ))
+            }
     </Layout>
     )
 };
+
+export const query = graphql`
+query {
+    allMdx(sort: {frontmatter: {date: DESC}}) {
+      nodes {
+        id
+        frontmatter {
+          title
+          date(formatString: "MMMM, D, YYYY")
+        }
+        excerpt
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="MyBlogPosts" />
 
